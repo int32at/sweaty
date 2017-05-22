8a3907f4ea593044203577077563378a1664ae97
Andreas Scharf <admin@andreas-scharf.at>
2017-05-22 12:05:42 +0200

    added support for key value pairs in dropdown

--- src/src/at/int32/sweaty/ui/controls/Dropdown.java
+++ src/src/at/int32/sweaty/ui/controls/Dropdown.java
@@ -1,9 +1,10 @@
 package at.int32.sweaty.ui.controls;
 
+import java.util.ArrayList;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 
@@ -14,8 +15,12 @@ import at.int32.sweaty.ui.annotations.OnValueChangedEvent;
 
 public class Dropdown extends Widget<Combo> {
 
+	private ArrayList<DropdownItem> items;
+
 	public Dropdown(Control parent) {
 		super(parent);
+
+		this.items = new ArrayList<DropdownItem>();
 
 		this.ctrl.addSelectionListener(new SelectionListener() {
 
@@ -23,7 +28,7 @@ public class Dropdown extends Widget<Combo> {
 			public void widgetSelected(SelectionEvent e) {
 
 				events.post(OnValueChanged.class,
-						new OnValueChangedEvent<String>(Dropdown.this,
+						new OnValueChangedEvent<DropdownItem>(Dropdown.this,
 								selection()));
 
 			}
@@ -34,8 +39,8 @@ public class Dropdown extends Widget<Combo> {
 		});
 	}
 
-	public String selection() {
-		return this.ctrl.getItem(this.ctrl.getSelectionIndex());
+	public DropdownItem selection() {
+		return items.get(this.ctrl.getSelectionIndex());
 	}
 
 	public Dropdown changed(Object o) {
@@ -51,37 +56,58 @@ public class Dropdown extends Widget<Combo> {
 		return c;
 	}
 
-	public Dropdown width(int width) {
-
+	public Dropdown add(String key, String value) {
+		this.ctrl.add(value);
+		this.items.add(new DropdownItem(key, value));
 		return this;
 	}
 
-	public Dropdown add(String item) {
-		this.ctrl.add(item);
-		return this;
+	public Dropdown add(DropdownItem item) {
+		return this.add(item.key(), item.value());
 	}
 
-	public Dropdown add(String... items) {
-		for (String item : items) {
+	public Dropdown add(DropdownItem... items) {
+		for (DropdownItem item : items) {
 			add(item);
 		}
 
 		return this;
 	}
 
-	public Dropdown select(String text) {
+	public Dropdown selectByItem(DropdownItem item) {
+		if (item == null)
+			selectByIndex(0);
+		else
+			selectByKey(item.key());
+		return this;
+	}
+
+	public Dropdown selectByValue(String value) {
 		String[] items = this.ctrl.getItems();
 
 		for (int i = 0; i < items.length; i++) {
-			if (items[i].equals(text)) {
-				select(i);
+			if (items[i].equals(value)) {
+				selectByIndex(i);
 				break;
 			}
 		}
 		return this;
 	}
 
-	public Dropdown select(int index) {
+	public Dropdown selectByKey(String key) {
+		for (int i = 0; i < items.size(); i++) {
+			DropdownItem item = items.get(i);
+
+			if (item.key().equals(key)) {
+				selectByIndex(i);
+				break;
+			}
+		}
+
+		return this;
+	}
+
+	public Dropdown selectByIndex(int index) {
 		this.ctrl.select(index);
 		return this;
 	}
